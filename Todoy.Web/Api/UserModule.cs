4133a6eb077719edfb1dbd71937b3b745c523de7
Luke Forder <luke.forder@gmail.com>
2014-12-11 22:24:05 +0200

    started user registration feature

---
+++ Todoy.Web/Api/UserModule.cs
@@ -0,0 +1,44 @@
+﻿using Common.Logging;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy.ModelBinding;
+
+namespace Todoy.Web.Api
+{
+    public class UserModule : NancyModule
+    {
+        public UserModule()
+        {
+            ILog log = LogManager.GetCurrentClassLogger();
+
+            Post["api/register", true] =
+                async (args, ct) =>
+                {
+                    //try
+                    //{
+                    //    Dto.RegisterDto registrationMessage = this.Bind<Dto.RegisterDto>();
+                    //}
+                    //catch  (Nancy.ModelBinding.ModelBindingException exception)
+                    //{
+                    //    // TODO: log the exception details
+
+                    //    return
+                    //        Negotiate
+                    //        .WithStatusCode(HttpStatusCode.BadRequest)
+                    //        .WithModel(
+                    //            new Dto.ErrorDto
+                    //            {
+                    //                Errors = new string[] { "Your registration details contained bad data :?" }
+                    //            });
+
+                    //}
+
+                    return null;
+                    
+                };
+        }
+    }
+}
